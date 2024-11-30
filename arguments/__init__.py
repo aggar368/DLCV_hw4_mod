d0ab450b9f24a60874b976832b94e855bf89b726
aggar368 <bnm9616700@yahoo.com.tw>
2024-11-30 21:38:09 +0800

    Hyperparam 2

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -88,7 +88,7 @@ class OptimizationParams(ParamGroup):
         self.exposure_lr_delay_mult = 0.0
         self.percent_dense = 0.01
         self.lambda_dssim = 0.2
-        self.densification_interval = 50 ######100
+        self.densification_interval = 200 ######100
         self.opacity_reset_interval = 3000
         self.densify_from_iter = 500
         self.densify_until_iter = 15_000
